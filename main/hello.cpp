586c3da754c4bac309e145252271ed133842a8e5
vinay7244 <95561432+vinay7244@users.noreply.github.com>
2023-02-18 10:55:27 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<<"PES1IG20CS701\n";
+  cout<<"PES1UG20CS701\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
